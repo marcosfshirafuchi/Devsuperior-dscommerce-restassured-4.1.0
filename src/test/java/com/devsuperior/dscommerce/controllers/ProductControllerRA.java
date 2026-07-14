7a0a5bf1a80727f4a00789debfda18a360f355ee
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-07-14 20:20:25 -0300

    Teste busca de produto por nome

--- src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -11,11 +11,13 @@ import static org.hamcrest.Matchers.*;
 public class ProductControllerRA {
 
     private Long existingProductId, nonExistingProductId;
+    private String productName;
 
     @BeforeEach
     public void sertUp() {
         //Endereço que vai estar hospedado o serviço
         baseURI = "http://localhost:8080";
+        productName = "Macbook";
     }
 
     //Exercícios de fixação: Testes de API com MockMvc
@@ -63,5 +65,58 @@ public class ProductControllerRA {
             .body("price", is(2190.0F))
             .body("categories.id", hasItems(2, 3))
             .body("categories.name", hasItems("Eletrônicos", "Computadores"));
+    }
+
+        /*Problema 2: Consultar produtos
+
+    Implemente os testes de API usando Rest Assured para consultar produtos (método GET do ProductController), considerando os seguintes cenários.
+    1.	Busca paginada exibe listagem paginada quando campo nome não preenchido e checa se os produtos Macbook Pro e PC Gamer Tera estão contidos
+    2.	Busca paginada filtra produtos por nome e exibe listagem paginada quando campo nome preenchidos
+    3.	Busca paginada filtra produtos de forma paginada e filtra produtos com preço maior que 2000.0
+
+    * */
+
+    //1.	Busca paginada exibe listagem paginada quando campo nome não preenchido e checa se os produtos Macbook Pro e PC Gamer Tera estão contidos
+    @Test
+    public void findAllShouldReturnPageProductsWhenProductNameIsEmpty(){
+        given()
+           //Está passando o endpoint para testar
+           .get("/products?page=0")
+        .then()
+           .statusCode(200)
+           //Verifica os nomes dos produtos
+           .body("content.name", hasItems("Macbook Pro", "PC Gamer Tera"));
+    }
+
+    //2.	Busca paginada filtra produtos por nome e exibe listagem paginada quando campo nome preenchidos
+    @Test
+    public void findAllShouldReturnPageProductsWhenProductNameIsNotEmpty(){
+        given()
+           //Está passando o endpoint para testar
+           .get("/products?name={productName}", productName)
+        .then()
+           //Verifica o status code
+           .statusCode(200)
+           //Verifica o id
+           .body("content.id[0]", is(3))
+           //Verifica o nome
+           .body("content.name[0]", equalTo("Macbook Pro"))
+           //Verifica o preço
+           .body("content.price[0]", is(1250.0F))
+           //Verifica a imagem
+           .body("content.imgUrl[0]", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
+    }
+
+    //3.	Busca paginada filtra produtos de forma paginada e filtra produtos com preço maior que 2000.0
+    @Test
+    public void findAllShouldReturnPagedProductsWithPriceGreaterThan2000(){
+        given()
+           //Está passando o endpoint para testar
+           .get("/products?size=25")
+        .then()
+           //Verifica o status code
+           .statusCode(200)
+           //Verifica os nomes dos produtos com o preço maior que 2000.0
+           .body("content.findAll {it.price > 2000}.name", hasItems("Smart TV", "PC Gamer Weed"));
     }
 }
